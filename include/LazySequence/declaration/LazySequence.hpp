be2402ce7b36e79940ffa1581e7ea2836424b457
ZIB00 <alexeypobedaabedoqyexela@gmail.com>
2026-06-05 19:02:59 +0300

    implementation of new Generator (now we have IGenerator, DefaultGenerator and ChangeGenerator)

--- include/LazySequence/declaration/LazySequence.hpp
+++ include/LazySequence/declaration/LazySequence.hpp
@@ -3,14 +3,14 @@
 #include <cstddef>
 #include <functional>
 #include <vcruntime_typeinfo.h>
+#include <memory>
 
 #include "../../Sequences/sequences/Sequence.hpp"
 #include "../../Sequences/sequences/ArraySequence.hpp"
-#include "../../Sequences/sequences/PriorityQueue.hpp"
-#include "../../Sequences/other/Option.hpp"
 
 enum Action {
         INSERT,
+        SET,
         REMOVE
 };
 
@@ -26,89 +26,155 @@ struct Change {
 };
 
 class Cardinal {
-    bool isInfinite; 
-    size_t value; 
+    private:
 
-    Cardinal(bool isInfinite, size_t val) : isInfinite(isInfinite), value(val) {}
+    bool isInfinite; 
+    size_t size; 
+
+    Cardinal(bool isInfinite, size_t val) : isInfinite(isInfinite), size(val) {}
 
     public:
 
-    Cardinal(size_t val) : Cardinal(false, val) {}
+    Cardinal(size_t size) : Cardinal(false, size) {}
     static Cardinal Infinite() { return Cardinal(true, 0); }
 
+    Cardinal operator+(size_t size) { 
+        return isInfinite ? Cardinal::Infinite() : Cardinal(false, this->size + size); 
+        }
+
+    Cardinal operator-(size_t size) { 
+        if(isInfinite) return Cardinal::Infinite();
+        return Cardinal(false, (this->size >= size) ? this->size - size : 0); 
+        }
+
     bool IsInfinite() const { return isInfinite; }
-    size_t GetValue() const { return value; }
+    size_t GetSize() const { return size; }
+};
+
+template<class T>
+class IGenerator
+{       
+        public:
+        virtual ~IGenerator() = default;
+        virtual const T& Get( size_t index ) = 0;
+        virtual Cardinal GetLength() = 0;
+};
+
+template<class T>
+class MainGenerator : public IGenerator<T>
+{       
+        private:
+        std::shared_ptr<ArraySequence<T>> list;
+        std::function<T(std::shared_ptr<Sequence<T>> list)> rule;
+        bool isInfinite;
+
+        public:
+        MainGenerator( std::shared_ptr<ArraySequence<T>> list, 
+                        std::function<T(std::shared_ptr<Sequence<T>> list)> rule ) 
+                                : list(list), rule(rule), isInfinite(true) {}
+
+        const T& Get( size_t index ) override {
+                if( list->GetLength() > index ) return list->Get(index);
+
+                try {
+                        while( list->GetLength() <= index ) {
+                                list->Append(rule(list));
+                        }
+
+                        return list->Get(index);
+                } catch (...) {
+                        isInfinite = false;
+                        throw OutOfRange( std::string("Index is out of range, sequense is finite, it lenght is: " + 
+                                std::to_string(list->GetLength())) );
+                }
+        }
+        Cardinal GetLength() override { 
+                return isInfinite ? Cardinal::Infinite() : Cardinal( list->GetLength() ); 
+        }
+};
+
+
+template<class T>
+class ChangeGenerator : public IGenerator<T>
+{
+        private:
+        std::shared_ptr<IGenerator<T>> prevGen;
+        Change<T> change;
+
+        public:
+        ChangeGenerator( std::shared_ptr<IGenerator<T>> prevGen, Change<T> change ) 
+                                : prevGen(prevGen), change(change) {}
+
+        const T& Get( size_t index ) override {
+                if( index == change.targetIndex ) {
+                        if( change.action == Action::SET || change.action == Action::INSERT ) {
+                                return change.value;
+                        }
+                        if( change.action == Action::REMOVE ) {
+                                return prevGen->Get(index + 1);
+                        }
+                }
+                if( index > change.targetIndex ) {
+                        if( change.action == Action::INSERT ) {
+                                return prevGen->Get(index - 1);
+                        }
+                        if( change.action == Action::REMOVE ) {
+                                return prevGen->Get(index + 1);
+                        }
+                }
+                return prevGen->Get(index);
+        }
+        Cardinal GetLength() override {
+                Cardinal prevLen = prevGen->GetLength();
+
+                if( change.action == Action::INSERT ) {
+                        return prevLen + 1;
+                }
+                if( change.action == Action::REMOVE ) {
+                        if( !prevLen.IsInfinite() && change.targetIndex >= prevLen.GetSize() ) {
+                                return prevLen;
+                        }
+                        return prevLen - 1;
+                }
+                return prevLen;
+        }
 };
 
 template< class T >
-class LazySequence : public Sequence<T>
+class LazySequence
 {
         private:
-        class Generator
-        {
-                private:
-                LazySequence<T>* parent;
-                std::function<T(Sequence<T>*)> rule;
-
-                unsigned currentIndex;
-                PriorityQueue<Change<T>, std::greater<T>> queue;
-
-                public:
-                Generator( LazySequence<T>* list, std::function<T(Sequence<T>*)> func ) 
-                        : parent(list), rule(func), currentIndex(0), queue(new PriorityQueue<T>) {} 
-                Generator( LazySequence<T>* list, const Generator& gen, const Change<T>& change );
-                Generator( LazySequence<T>* list, const Generator& other ) 
-                        : parent(list), rule(other.rule) {}
-                ~Generator() { if(parent) delete parent; }
-
-                T GetNext();
-                bool HasNext() const;
-                Option<T> TryGetNext();
-
-                Generator* Append( T item )             const { return new Generator( parent, *this, { currentIndex, item, Action::INSERT} ); }
-                Generator* Append( Sequence<T>* items ) const;
-                Generator* Set( size_t index, T item ) const { return new Generator( parent, *this, { index, item, Action::INSERT} ); }
-                Generator* Insert( size_t index, T item )             const { return new Generator( parent, *this, { index, item, Action::INSERT} ); }
-                Generator* Insert( size_t index, Sequence<T>* items ) const;
-                Generator* Remove( size_t index, T item)              const { return new Generator( parent, *this, { index, item, Action::REMOVE} ); }
-                Generator* Remove( size_t index, Sequence<T>* items ) const;
-        };
-
-        bool isInfinite;
         MutableArraySequence<T> list;
-        Generator* generator;
+        std::shared_ptr<IGenerator<T>> generator;
+
+        LazySequence( const LazySequence<T>& list, std::shared_ptr<IGenerator<T>> newGen ) 
+                : list(list.list), generator(newGen) {}
 
         public:
         using value_type = T;
 
-        LazySequence() : isInfinite(false), list(), generator(nullptr) {}
-        LazySequence( T* items, int count ) 
-                : isInfinite(false), list( items, static_cast<size_t>(count) ), generator(nullptr) {}
-        LazySequence( Sequence<T>* seq )    : isInfinite(false), list(seq), generator(nullptr) {}
+        LazySequence() : list(), generator(nullptr) {}
+        LazySequence( T* items, int count ) : list( items, static_cast<size_t>(count) ), generator(nullptr) {}
+        LazySequence( Sequence<T>* seq )    : list(seq), generator(nullptr) {}
         //LazySequence( T(*func)(Sequence<T>*), Sequence<T>* seq ) : isInfinite(true), list(seq), generator(new Generator(this, func)) {} Зачем?
         LazySequence( std::function<T(Sequence<T>*)> func, Sequence<T>* seq ) 
-                : isInfinite(true), list(seq), generator(nullptr) { if(func) this->generator = new Generator(this, func); }
+                : list(seq), generator(nullptr) { if(func) this->generator = std::make_shared<IGenerator>(this, func); }
         LazySequence( const LazySequence<T>& list ) 
-                : isInfinite(list.isInfinite), list(list.list), generator(nullptr) { if(list.generator) this->generator = new Generator(this, *(list.generator)); }
-        ~LazySequence() { if (generator) delete generator; };
+                : list(list.list), generator(nullptr) { if(list.generator) this->generator = std::make_shared<IGenerator>(this, *(list.generator)); }
+        ~LazySequence() = default;
 
-        T GetFirst() override;
-        T GetLast() override;
-        const T& Get(size_t index) override;
-        void Set( size_t index, T value) override { generator.; }
-        LazySequence<T>* GetSubsequence( size_t startIndex, size_t endIndex ) override;
-        Cardinal GetLength() override { return isInfinite ? Cardinal::Infinite() : Cardinal(list.GetLength()); }
-        size_t GetMaterializedCount() const; 
-
-        Sequence<T>* Append(T item) override;
-        Sequence<T>* Prepend(T item) override;
-        Sequence<T>* InsertAt( T item, size_t index ) override;
-        LazySequence<T>* Concat( LazySequence<T>* list ) override;
-
-        Sequence<T>* CreateEmpty() override;
+        T GetFirst() { return Get(0); }
+        T GetLast()  { return Get(list.GetLength() - 1); }
+        const T& Get(size_t index) { return generator->Get(index); }
+        Cardinal GetLength() { return generator->GetLength(); }
+        size_t GetMaterializedCount() const { return list.GetLength(); } 
         
-        IEnumerator<T>* GetEnumerator() override;
+        LazySequence<T>* Set( size_t index, T value );
+        LazySequence<T>* GetSubsequence( size_t startIndex, size_t endIndex );
+        LazySequence<T>* Append(T item);
+        LazySequence<T>* Prepend(T item);
+        LazySequence<T>* InsertAt( T item, size_t index );
+        LazySequence<T>* Concat( LazySequence<T>* list );
 };
 
-#include "../implementation/Generator.tpp"
 #include "../implementation/LazySequence.tpp"
--- include/LazySequence/implementation/LazySequence.tpp
+++ include/LazySequence/implementation/LazySequence.tpp
@@ -2,30 +2,3 @@
 #include <cstddef>
 #include <string>
 
-template <class T>
-const T& LazySequence<T>::Get(size_t index) {
-    if (index < (this->list.GetLength())) return list.Get(index);
-
-    if (!isInfinite || !generator) {
-        std::string err{ "Sequence is finite and index is out of range: " + std::to_string(index) + \
-            ">" + std::to_string(this->list.GetLength()) };
-        throw OutOfRange(err);
-    }
-
-    while (list.GetLength() <= index) {
-        Option<T> value = generator->TryGetNext();
-        if (!value) {
-            isInfinite = false;
-            std::string err{ "IndexOutOfRange: value cannot be generated anymore" };
-            throw OutOfRange(err);
-        }
-        list.Append(value.Value());
-    }
-
-    return list.Get(index);
-}
-
-template<class T>
-T LazySequence<T>::GetFirst() {
-    return Get(0);
-}
